ab69d4430d27df8c302c4e090d94b6c5a566cac2
sunimalherath <sunimaldv@gmail.com>
2021-01-07 20:20:09 +1100

    make improvements to go routine

--- with-channels-improved.go
+++ with-channels-improved.go
@@ -22,23 +22,23 @@ func getPage(url string) (int, error) {
 	return len(body), nil
 }
 
-func getter(url string, size chan string) {
+func getter(url string, size chan int) {
 	length, err := getPage(url)
 	if err == nil {
-		size <- fmt.Sprintf("%s => page size: %d", url, length)
+		size <- length
 	}
 }
 
 func main() {
 	urls := []string{"http://www.apple.com", "http://www.microsoft.com", "http://www.samsung.com"}
 
-	size := make(chan string)
+	size := make(chan int)
 
 	for _, url := range urls {
 		go getter(url, size)
 	}
 
 	for i := 0; i < len(urls); i++ {
-		fmt.Printf("%s\n", <-size)
+		fmt.Printf("%s => page size: %d\n", urls[i], <-size)
 	}
 }
